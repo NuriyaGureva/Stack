5e2adac1d8a305072cfe9d151e37b8376fcf5b9e
gureva.anj@gmail.com <gureva.anj@gmail.com>
2022-02-25 14:18:42 +0300

    Добавьте файлы проекта.

---
+++ Stack/Stack.cpp
@@ -0,0 +1,65 @@
+﻿// Stack.cpp : 
+#include <iostream>
+using namespace std;
+#include<stack>
+#define tab "\t"
+
+class Stack 
+{
+	struct Node
+	{
+		int Data;
+		Node* pNext;
+	public:
+		Node(int Data, Node* pNext = nullptr) : Data(Data), pNext(pNext){}
+		~Node()	{}
+
+	}*Head;
+
+public:
+
+	Stack()
+	{
+		Head == nullptr;
+		
+	}
+	~Stack() {}
+
+	void push(int Data)
+	{
+		Head = new Node(Data, Head);
+		//Node* Temp = new Node(Data);
+		//Temp->pNext = Head;
+		//Head = Temp;
+		
+	}
+	void pop_front()
+	{
+		if (Head == nullptr)return;		
+		Node* Temp = Head;		
+		Head = Temp->pNext;	
+		delete Temp;		
+	}
+
+	bool empty()
+	{
+		return Head == nullptr;
+	}	
+	
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	stack<int> s;
+	for (int i =1; i<10; i++)
+		s.push(i);
+	
+	while (!s.empty()) {
+		cout << s.top() << endl;
+		s.pop();
+	}	
+	
+}
+
